da6ecfb275c4cc92c41c74fdfcb02e88545ea745
BotChinu <banda.shri1998@gmail.com>
2024-10-04 14:43:35 +0530

    location changed

--- chapter 10/exercise 10/JS/10-f.js
+++
@@ -1,9 +0,0 @@
-function toggle(selector){
-  const btn=document.querySelector(selector);
-  if(!btn.classList.contains('is-toggled')){
-    btn.classList.add('is-toggled');
-  }
-  else{
-    btn.classList.remove('is-toggled');
-  }
-}
