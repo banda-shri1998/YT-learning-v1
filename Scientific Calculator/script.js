5186464b716d2b921d678ecf5d64d8dcff985770
BotChinu <banda.shri1998@gmail.com>
2024-10-10 12:14:07 +0530

    completed basic functionality of calculator

---
+++ Scientific Calculator/script.js
@@ -0,0 +1,17 @@
+const display = document.querySelector("input");
+let c = '';
+const btn = document.querySelectorAll(".btn").forEach((item) => {
+  display.value = ''
+  item.addEventListener("click", () => {
+    if (item.innerHTML === 'AC') {
+      display.value = "";
+    }
+    else if (item.innerHTML === '=') {
+      display.value = (eval(display.value)).toString();
+    }
+    else {
+      display.value += item.innerHTML
+      console.log(c);
+    }
+  });
+});
